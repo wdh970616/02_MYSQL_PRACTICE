9b11f29714b2bc05c2946daa570ed8c311f57560
wdh970616 <wdh970616@naver.com>
2024-06-11 09:54:52 +0900

    WHERE 추가문제

--- 03_WHERE_HW.sql
+++ 03_WHERE_HW.sql
@@ -9,12 +9,12 @@ where salary >= 5000000;
 select *
 from employee
 where dept_code = 'D1'
-and salary >= 3000000;
+  and salary >= 3000000;
 
 select *
 from employee
 where dept_code = 'D1'
-or job_code = 'J4';
+   or job_code = 'J4';
 
 select *
 from employee
@@ -60,3 +60,31 @@ where dept_code is null;
 select *
 from employee
 where email is not null;
+
+-- 추가문제
+select emp_name,
+       dept_title,
+       salary
+from employee,
+     department
+where dept_code in ('D6', 'D8');
+
+select emp_id,
+       emp_name,
+       email
+from employee
+where emp_name not like '이%';
+
+select emp_name,
+       salary,
+       job_code
+from employee
+where job_code = 'J2' and salary >= 2000000
+   or job_code = 'J7';
+
+select emp_name,
+       salary,
+       job_code
+from employee
+where job_code in ('J7', 'J2')
+  and salary >= 2000000;
